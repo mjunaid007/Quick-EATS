3d7cbb6e347978514627c20bb99cb0bc4bd046b9
M Junaid Nasir <156563939+mjunaid007@users.noreply.github.com>
2024-08-25 19:08:06 +0530

    Update StoreContext.jsx

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const url = "http://localhost:4000"
+    const url = "https://quick-eats-backend-8e18.onrender.com"
     const [food_list, setFoodList] = useState([]);
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("")
